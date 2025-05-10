bc61dfef1e2def8e1f3692c957492f0a1da36d47
Milo Sanders <admin@autumnomous.com>
2025-05-10 10:04:05 -0400

    setup basic server

---
+++ api/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening on port ${PORT}`);
+});
